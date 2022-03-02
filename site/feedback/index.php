589b6a7d13914b8e7533ebf4600b683705dac484
koushiks11 <koushiks.is20@bmsce.ac.in>
2022-03-02 17:12:16 +0530

    Feedback

---
+++ site/feedback/index.php
@@ -0,0 +1,89 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pack Your Bags</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Elegant Feedback Form  Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //custom-theme -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@3006display-swap" rel="stylesheet">
+<link rel="stylesheet" href="style1.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style type="text/css">
+	.home {
+		font-family: fantasy;
+	}
+</style>
+</head>
+<body class="agileits_w3layouts">
+	
+  <header id="header-bar">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.html">
+      <img src="logo.png" alt="" width="60" height="60">
+      <span>PACK YOUR BAGS</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active home" aria-current="page" href="../index.html" style="color: #fff">Home</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
+
+<!-- End of header -->
+
+    <h1 class="agile_head text-center">Feedback Form</h1>
+    <div class="w3layouts_main wrap">
+	  <h3>Please help us to serve you better by taking a couple of minutes. </h3>
+	    <form action="feedback.php" method="post" class="agile_form">
+		  <h2>How satisfied were you with our Service?</h2>
+			<textarea placeholder="Comments" class="w3l_summary" name="comments" required=""></textarea>
+			<input type="text" placeholder="Name*" name="name"  required/>
+			<input type="email" placeholder="Email*" name="email" required/>
+			<input type="text" placeholder="Number(optional)" name="num"/><br>
+			<center><input type="submit" value="Submit" class="agileinfo" /></center>
+	  </form>
+	</div>
+	<!-- Footer -->
+<br style="height: 10px">
+<div class="footer">
+  <footer>
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="15" height="14"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <span class="text-muted" > Pack Your Bags, Inc</span>
+<span class="icons"><a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-instagram"></a></span>
+    </div>
+  </footer>
+</div>
+
+  
+
+<!-- End of Footer -->
+</body>
+</html>
+
